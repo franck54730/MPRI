8a1322aff2a7d4fec1521e7cf3efa91d5f685e84
Franck54730 <franck_audiger@hotmail.com>
2015-12-19 10:45:42 +0100

    projet marche

--- face_learning.m
+++ face_learning.m
@@ -1,50 +1,53 @@
-function face_learning
-%  Identifier les motifs globaux, G_Patterns, prÃ©sents dans toutes les
+function face_learning()
+%  Identifier les motifs globaux, G_Patterns, présents dans toutes les
 %  images de tous les visages de la base de connaissance ainsi que les
 %  occurrences de ces motifs dans chaque image de chaque visage de la base.
 %  
-%  Chaque image est dÃ©coupÃ©e en N blocs 4x4. Chaque bloc est analysÃ© en
-%  frÃ©quences via la DCT2 dont le coefficient DC est ignorÃ©. Ensuite chaque
-%  image est rÃ©prÃ©sentÃ©e par une matrice Nx15 qui sert Ã  la fois Ã 
-%  identifier les motifs globaux et Ã  construire l'histogramme de ces
+%  Chaque image est découpée en N blocs 4x4. Chaque bloc est analysé en
+%  fréquences via la DCT2 dont le coefficient DC est ignoré. Ensuite chaque
+%  image est réprésentée par une matrice Nx15 qui sert à la fois à
+%  identifier les motifs globaux et à construire l'histogramme de ces
 %  motifs pour chaque image de chaque visage de la base de connaissance.
 %
-% Les paramÃ¨tres dont les motifs globaux, utiles Ã  l'identification d'une
-% image inconnue sont enregistrÃ©s dans le fichier 'params.mat' ainsi que
+% Les paramètres dont les motifs globaux, utiles à l'identification d'une
+% image inconnue sont enregistrés dans le fichier 'params.mat' ainsi que
 % les histogrammes de chaque image de chaque visage de la base de
 % connaissance.
 %
-% Le mÃªme traitement peut Ãªtre fait avec les coefficients DC. Mais ils sont
-% ignorÃ©s pour ce projet.
+% Le même traitement peut être fait avec les coefficients DC. Mais ils sont
+% ignorés pour ce projet.
 
-%% RÃ©initialiser l'espace de travail
+%% Réinitialiser l'espace de travail
 clear
 clc
 
-%% DÃ©finir le rÃ©pertoire de la base de connaisssance #
+%% Définir le répertoire de la base de connaisssance #
 db_path = uigetdir();
 
-%% Initialiser les paramÃ¨tres globaux
+%% Initialiser les paramètres globaux
 BSZ = 4;
 QP = 22;
 N_AC_PATTERNS = 35;
-NB_FACES = 1;
-NB_IMAGES = 1;
+NB_FACES = 5;
+NB_IMAGES = 5;
+NB_FACES_RECONNAISSANCE = 20;
+NB_IMAGES_RECONNAISSANCE = 5;
 DC_MEAN_ALL = 0;
 
 %% Extraire les N blocs DCT pour chaque image de chaque visage
-AC_list = cell(NB_FACES,NB_IMAGES);% les matrices Nx15
-AC_list_after = cell(NB_FACES,NB_IMAGES);%apres calcul
-dc_means = zeros(NB_FACES,NB_IMAGES);% les moyennes par image des DC
+AC_list = cell(NB_FACES_RECONNAISSANCE,NB_IMAGES_RECONNAISSANCE);% les matrices Nx15
+AC_list_after = cell(NB_FACES_RECONNAISSANCE,NB_IMAGES_RECONNAISSANCE);%apres calcul
+dc_means = zeros(NB_FACES_RECONNAISSANCE,NB_IMAGES_RECONNAISSANCE);% les moyennes par image des DC
 % des constantes
 blocSz =  (1:BSZ) - 1;
 BZ2 = floor(BSZ/2);
 ACSZ = BSZ * BSZ - 1;
+
 % pour chaque visage
-for f = 1:NB_FACES
+for f = 1:NB_FACES_RECONNAISSANCE
     face_path = sprintf('%s/base_connaissance/s%d',db_path,f);
     % pour chaque image
-    for fi = 1:NB_IMAGES
+    for fi = 1:NB_IMAGES_RECONNAISSANCE
         fname = sprintf('%s/%d.png',face_path,fi);
         img = imread(fname);
         [h,w] = size(img);
@@ -54,25 +57,25 @@ for f = 1:NB_FACES
             for j=1:2:(w-3)
                 n_blocks = n_blocks+1;
                 aux = dct2(img(i:(i+3),j:(j+3)));
-                aux = aux(:);
-                AC_list{f,fi}{n_blocks} = aux(2:16);
-                dc_somme = dc_somme+aux(1);
+                %aux = aux(:);
+                dc_somme = dc_somme+aux(1,1);
+                tmp(n_blocks, 1:ACSZ) = aux(2:16);
             end
         end
-        %AC_list{f,fi}(n_blocks) = tmp;
-        dc_means(f,fi) =dc_somme/n_blocks;
+        AC_list{f,fi} = tmp;
+        dc_means(f,fi) = dc_somme/n_blocks;
     end
 end
-dc_means
 DC_MEAN_ALL = mean2(dc_means);
-DC_MEAN_ALL
-%% Stockage des paramÃ¨tres dans une structure
+%% Stockage des paramètres dans une structure
 params = struct(...
     'BZS',BSZ,...
     'QP',QP,...
     'N_AC_PATTERNS',N_AC_PATTERNS,...
     'NB_FACES',NB_FACES,...
     'NB_IMAGES',NB_IMAGES,...
+    'NB_FACES_RECONNAISSANCE',NB_FACES_RECONNAISSANCE,...
+    'NB_IMAGES_RECONNAISSANCE',NB_IMAGES_RECONNAISSANCE,...
     'DC_MEAN_ALL',DC_MEAN_ALL,...
     'DIR',db_path);
 
@@ -82,84 +85,70 @@ disp('dct done');
 
 %% Identification des motifs globaux, construction de leurs histogrammes
 G_Patterns = [];
+for f = 1:NB_FACES_RECONNAISSANCE
+    for fi = 1:NB_IMAGES_RECONNAISSANCE
+        % normalisation et quantification des AC
+        List_PatternAC_current_image = AC_list{f,fi};
+        h= size(AC_list{f,fi},1);
+        for i = 1:h
+            for j = 1:ACSZ
+                a = List_PatternAC_current_image(i,j)*DC_MEAN_ALL;
+                b=a/dc_means(f,fi)/QP;
+                tmp(i,j) = round(b);
+            end
+            QAC(i,1:ACSZ)=tmp(i,1:ACSZ);
+        end
+        AC_list_after{f,fi} = QAC; 
+    end
+end
+
+
+% identification des motifs et comptage de leurs occurrences.
+        % QAC est la matrice des vecteurs AC quantifés
 for f = 1:NB_FACES
     for fi = 1:NB_IMAGES
-        % normalisation et quantification des AC
-        aux = AC_list{f,fi};
-        [h,w] = size(aux);
-        list = cell(w); 
-        for i = 1:w
-            a = aux{i}*DC_MEAN_ALL;
-            b=a/dc_means(f,fi)/QP;
-            aux_AC = round(b);
-            list{i} = aux_AC;
-        end
-        
-        
-        size(list,1)
-        AC_list_after{f,fi} = zeros(size(list,1),ACSZ);
-        for i = 1:size(list,1)
-            
-        end
-        
-        
-        
-        
-        % identification des motifs et comptage de leurs occurrences.
-				% QAC est la matrice des vecteurs AC quantifÃ©s
-        if(size(G_Patterns,1) == 0)
-            G_Patterns(1,1:ACSZ) = list{1}(1:ACSZ);
+        if(f==1 && fi == 1)
+            G_Patterns(1,1:ACSZ) = AC_list_after{f,fi}(1,1:ACSZ);
             G_Patterns(1,ACSZ+1) = 1;
         else
-            findtab = ismember(G_Patterns(:,1:ACSZ), list{i}(1:ACSZ)','rows');
-            somme = sum(findtab);
-            index = find(findtab,1);
-            G_Patterns(size(G_Patterns,1),1:ACSZ) = list{i}(1:ACSZ);
-            if somme > 0
-                G_Patterns(index,ACSZ+1) = G_Patterns(index,ACSZ+1)+1;
-            else
-                G_Patterns(size(G_Patterns,1)+1,1:ACSZ) = list{i}(1:ACSZ);
-                G_Patterns(size(G_Patterns,1)+1,ACSZ+1) = 1;
-            end
-        end
-        for i = 2:size(list,1)
-            findtab = ismember(G_Patterns(:,1:ACSZ), list{i}(1:ACSZ)','rows');
-            somme = sum(findtab);
-            index = find(findtab,1);
-            G_Patterns(size(G_Patterns,1),1:ACSZ) = list{i}(1:ACSZ);
-            if somme > 0
-                G_Patterns(index,ACSZ+1) = G_Patterns(index,ACSZ+1)+1;
-            else
-                G_Patterns(size(G_Patterns,1)+1,1:ACSZ) = list{i}(1:ACSZ);
-                G_Patterns(size(G_Patterns,1)+1,ACSZ+1) = 1;
+            for i = 1:size(AC_list_after{f,fi},1)
+                findtab = ismember(G_Patterns(:,1:ACSZ), AC_list_after{f,fi}(i,1:ACSZ),'rows');
+                somme = sum(findtab);
+                index = find(findtab,1);
+                %G_Patterns(size(G_Patterns,1),1:ACSZ) = QAC{i}(1:ACSZ);
+                if somme > 0
+                    G_Patterns(index,ACSZ+1) = G_Patterns(index,ACSZ+1)+1;
+                else
+                    G_Patterns(size(G_Patterns,1)+1,1:ACSZ) = AC_list_after{f,fi}(i,1:ACSZ);
+                    G_Patterns(size(G_Patterns,1)+1,ACSZ+1) = 1;
+                end
             end
         end
     end
 end
-AC_list_after{1,1}{50}
-% Conserver les N_AC_PATTERNS motifs les plus prÃ©sents dans toutes les
+% Conserver les N_AC_PATTERNS motifs les plus présents dans toutes les
 % images de tous les visages de la base.
 [~,Idx] = sort(G_Patterns(:,end),'descend');
-G_Patterns = G_Patterns(Idx(1:N_AC_PATTERNS),1:(end-1));
-
+G_Patterns = G_Patterns(Idx(1:N_AC_PATTERNS),1:(end));
+figure;
+plot(G_Patterns)
 
 % save G_Patterns
-save('G_Patterns.mat','G_Patterns')
-disp('G_Patterns done')
+save('G_Patterns.mat','G_Patterns');
+disp('G_Patterns done');
 
 %% Construction des histogrammes de toutes les images de chaque visage
-AC_Patterns_Histo_List = cell(NB_FACES,NB_IMAGES);
-
-for f = 1:NB_FACES
-    for fi = 1:NB_IMAGES
-        AC_Patterns_Histo = zeros(N_AC_PATTERNS,1);
+AC_Patterns_Histo_List = cell(NB_FACES_RECONNAISSANCE,NB_IMAGES_RECONNAISSANCE);
+AC_Patterns_Histo = zeros(N_AC_PATTERNS,1);
+for f = 1:NB_FACES_RECONNAISSANCE
+    for fi = 1:NB_IMAGES_RECONNAISSANCE
         for i = 1:N_AC_PATTERNS
-            AC_Patterns_Histo(i) = ismember(AC_list_after{f,fi}{:}, G_Patterns(i,1:ACSZ),'rows');
-            %AC_Patterns_Histo(i)
+           AC_Patterns_Histo(i) = sum(ismember(AC_list_after{f,fi}(:,1:15), G_Patterns(i,1:ACSZ),'rows'));
         end
         AC_Patterns_Histo_List{f,fi} = AC_Patterns_Histo;
     end
 end
+figure;
 %save AC_Patterns_Histo_List
-save('AC_Patterns_Histo_List.mat','AC_Patterns_Histo_List')
+save('AC_Patterns_Histo_List.mat','AC_Patterns_Histo_List');
 disp('AC_Patterns_Histo_List done');
--- face_recognition.m
+++ face_recognition.m
@@ -16,6 +16,8 @@ QP = params.QP
 N_AC_PATTERNS = params.N_AC_PATTERNS
 NB_FACES = params.NB_FACES
 NB_IMAGES = params.NB_IMAGES
+NB_FACES_RECONNAISSANCE = params.NB_FACES_RECONNAISSANCE
+NB_IMAGES_RECONNAISSANCE = params.NB_IMAGES_RECONNAISSANCE
 DC_MEAN_ALL = params.DC_MEAN_ALL
 DIR = params.DIR
 %load('G_Patterns.mat');
@@ -24,33 +26,56 @@ DIR = params.DIR
 %% extraction des blocs DCT
 [h,w] = size(img);
 n_blocks = 0;
-width_blocks = floor((w-1)/2);
-height_blocks = floor((h-1)/2);
-A = zeros(height_blocks,width_blocks);
-for i=1:1:height_blocks
-    for j=1:1:height_blocks
+dc_somme = 0;
+for i=1:2:(h-3)
+    for j=1:2:(w-3)
         n_blocks = n_blocks+1;
-        dct = dct2(img(i:(i+3),j:(j+3)));
-        A(i,j) = dct;
+        aux = dct2(img(i:(i+3),j:(j+3)));
+        %aux = aux(:);
+        AC_image(n_blocks,:) = aux(2:16);
+        dc_somme = dc_somme+aux(1);
     end
 end
 
 %% Normalisation et quantification
-
+dc_means=dc_somme/size(AC_image,1);
+for i=1:size(AC_image,1)
+    a = AC_image(i,:)*DC_MEAN_ALL;
+    b=a/dc_means/QP;
+    AC_image(i,:) = round(b);
+end
 
 %% Comptage des occurrences des motifs globaux
 load('G_Patterns.mat');
 AC_Signatures = zeros(N_AC_PATTERNS,1);
 for idx = 1:N_AC_PATTERNS
-    AC_Signatures(idx) = find_Pattern(G_Patterns(idx,:),QAC);
+    AC_Signatures(idx) = sum(ismember(AC_image(:,1:15),G_Patterns(idx,1:15) ,'rows'));
+    %AC_Signatures(idx) = find_Pattern(G_Patterns(idx,1:15),QAC);
 end
-
 %% Sélection des KPP meilleures AC_Patterns_Histo par PVH
-best = ones(KPP+1,3)*-1; % chaque ligne est <SAD,N°individu,N°profil>
-%% CUT HERE ====================================================================
-%% CUT HERE ====================================================================
-best = best(1:(end-1),2:end);
-
+load('AC_Patterns_Histo_List.mat');
+SAD_list = zeros(NB_FACES_RECONNAISSANCE,NB_IMAGES_RECONNAISSANCE);
+for f = 1:NB_FACES_RECONNAISSANCE
+    for fi = 1:NB_IMAGES_RECONNAISSANCE
+        AC_Patterns_Histo = AC_Patterns_Histo_List{f,fi};
+        city_block = 0;
+        for i=1:N_AC_PATTERNS
+            val = abs(AC_Patterns_Histo(i)-AC_Signatures(i));
+            city_block = city_block + val;
+        end
+        SAD_list(f,fi) = city_block;
+    end
+end
+best = ones(KPP,3)*-1; % chaque ligne est <SAD,N°individu,N°profil>
+for i=1:KPP  
+    [ligne,indice_face] = min(SAD_list);
+    [valeur,indice_image] = min(ligne);
+    best(i,1) = valeur;
+    best(i,3) = indice_image+5;
+    best(i,2) = indice_face(indice_image);
+    SAD_list(indice_face,indice_image) = intmax;
+end
+best = best(:,2:end);
 %% visualisation des visages possiblement identifiés
 if( visu)
     figure;
@@ -58,8 +83,7 @@ if( visu)
     imshow(img);
     for b = 1:KPP
         subplot(1,KPP+1,b+1);
-        filename = sprintf('%s/s%d/%d.png',...
-            params.DIR,best(b,1),best(b,2));
+        filename = sprintf('%s/base_tests/s%d/%d.png',params.DIR,best(b,1),best(b,2));
         imreco = imread(filename);
         imshow(imreco);
     end
